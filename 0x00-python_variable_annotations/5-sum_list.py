2a8f251621d5127cf5b6dd50bb83c39307285c8a
Anasou1 <anasoudsassi@gmail.com>
2024-04-18 13:29:46 +0100

    updating 5-sim_list.py file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''type-annotated function sum_list'''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''function that return sum of a list'''
     return float(sum(input_list))
