f0009f54c18e65555d74238da6a8cfb7cec6e4ee
Anasou1 <anasoudsassi@gmail.com>
2024-04-23 13:57:44 +0100

    adding type_annotation to 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float]:
     '''function that loop 10 times and yield a random number (0<=x<=10)'''
     for i in range(10):
         await asyncio.sleep(1)
